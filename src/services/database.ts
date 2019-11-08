d486c4afc60be2c6da06dbb3bbac4f4680b8c5f8
Ruan Beukes <ruan.coder@gmail.com>
2019-11-08 16:53:51 +0800

    patch load by id works

--- src/services/database.ts
+++ src/services/database.ts
@@ -30,42 +30,49 @@ export const patchUpdate = async <TRecord>(
   value: PatchLoanModel
 ): Promise<TRecord | undefined> => {
 
-/*
-// This is to get it working with the basics
-const params: UpdateItemInput = {
-    TableName: tableName,
-    Key: { id } as any, // cast to shut up typescript
-    UpdateExpression: `SET #status = :status, #comment = :comment`,
-    ExpressionAttributeNames: { '#status': 'status', '#comment': 'comment' },
-    ExpressionAttributeValues: {
-      ':status': value.status as any, // cast to shut up typescript
-      ':comment': value.comment as any // cast to shut up typescript
-    },
-    ReturnValues: 'ALL_NEW'
-  }
-*/
+  /*
+  // This is to get it working with the basics
+  const params: UpdateItemInput = {
+      TableName: tableName,
+      Key: { id } as any, // cast to shut up typescript
+      UpdateExpression: `SET #status = :status, #comment = :comment`,
+      ExpressionAttributeNames: { '#status': 'status', '#comment': 'comment' },
+      ExpressionAttributeValues: {
+        ':status': value.status as any, // cast to shut up typescript
+        ':comment': value.comment as any // cast to shut up typescript
+      },
+      ReturnValues: 'ALL_NEW'
+    }
+  */
 
   let updateExpression = `#${nameof<PatchLoanModel>('status')} = :${nameof<PatchLoanModel>('status')}`;
+  let expressionAttributeNames = `"#${nameof<PatchLoanModel>('status')}": "${nameof<PatchLoanModel>('status')}"`;
+  let expressionAttributeValues = `":${nameof<PatchLoanModel>('status')}": "${value.status}"`;
 
-  if  (value.comment && value.comment.trim()){
+  if (value.comment && value.comment.trim()) {
     updateExpression += `, #${nameof<PatchLoanModel>('comment')} = :${nameof<PatchLoanModel>('comment')}`;
+    expressionAttributeNames += `, "#${nameof<PatchLoanModel>('comment')}": "${nameof<PatchLoanModel>('comment')}"`;
+    expressionAttributeValues += `, ":${nameof<PatchLoanModel>('comment')}": "${value.comment}"`;
   }
+  
+  expressionAttributeNames = `{${expressionAttributeNames}}`;
+  expressionAttributeValues = `{${expressionAttributeValues}}`
 
+  console.log(updateExpression);
+  console.log(expressionAttributeNames);
+  console.log(expressionAttributeValues);
 
   const params: UpdateItemInput = {
     TableName: tableName,
     Key: { id } as any, // cast to shut up typescript
-    UpdateExpression: `SET #status = :status, #comment = :comment`,
-    ExpressionAttributeNames: { '#status': 'status', '#comment': 'comment' },
-    ExpressionAttributeValues: {
-      ':status': value.status as any, // cast to shut up typescript
-      ':comment': value.comment as any // cast to shut up typescript
-    },
+    UpdateExpression: `SET ${updateExpression}`,
+    ExpressionAttributeNames: JSON.parse(expressionAttributeNames),
+    ExpressionAttributeValues: JSON.parse(expressionAttributeValues),
     ReturnValues: 'ALL_NEW'
   }
+  
   const docClient = new DynamoDB.DocumentClient(createDocumentClientOptions());
 
   const result = await docClient.update(params).promise();
-
   return result.Attributes as TRecord;
 };
