9bc864bf4da0db470167edbfdfdc0a802cc5017a
Ruan Beukes <ruan.coder@gmail.com>
2019-11-27 14:53:15 +0800

    create cognito test user: pass variables username and password instead of using the default values.

--- deployment-scripts/create.cognito.test.user.js
+++ deployment-scripts/create.cognito.test.user.js
@@ -4,18 +4,24 @@ const cognitoidentityserviceprovider = new AWS.CognitoIdentityServiceProvider({ 
 const environment = (process.argv && process.argv[2] && process.argv[2].toLowerCase()) || 'dev';
 console.log(`Environment: ${environment}`);
 
-const userPoolId = process.argv && process.argv[3] && process.argv[3];
+const userPoolId = process.argv && process.argv[3];
 console.log(`userPoolId: ${userPoolId}`);
 
+const username = process.argv && process.argv[4];
+console.log(`username: ${username}`);
+
+const password = process.argv && process.argv[5];
+console.log(`password: ${password}`);
+
 if (environment === 'dev' || environment === 'test') {
-  testUser().create$(environment);
+  testUser().create$(environment, username, password);
 } else {
-  console.log("No need to add 'test@test.com' to the cognito user pool.");
+  console.log(`No need to add test user (${username || 'test@test.com'}) to the cognito user pool `);
 }
 
 function testUser() {
 
-  const create$ = async environment => {
+  const create$ = async (environment, username, password) => {
     environment = environment && environment.toLowerCase();
 
     if (userPoolId) {
@@ -26,7 +32,7 @@ function testUser() {
           'email'
           /* more items */
         ],
-        Filter: "email='test@test.com'"
+        Filter: `email='${username || 'test@test.com'}'`
       };
       cognitoidentityserviceprovider.listUsers(listUsersParams, function (err, data) {
         if (err) {
@@ -37,7 +43,7 @@ function testUser() {
 
           if (users.length > 0) {
             console.log(
-              `*************** Nothing to do, 'test@test.com' user already exist in '${userPoolId})'`
+              `*************** Nothing to do, '${username || 'test@test.com'}' user already exist in '${userPoolId})'`
             );
           } else {
             //add the test user
@@ -56,11 +62,11 @@ function testUser() {
             //add cognito users
             let params = {
               UserPoolId: userPoolId /* required */,
-              Username: `test@test.com` /* required */,
+              Username: username || 'test@test.com' /* required */,
               DesiredDeliveryMediums: ['EMAIL'],
               ForceAliasCreation: false,
               MessageAction: 'SUPPRESS',
-              TemporaryPassword: 'passwordpassword',
+              TemporaryPassword: password || 'passwordpassword',
               UserAttributes: [
                 {
                   Name: 'email_verified' /* required */,
@@ -68,7 +74,7 @@ function testUser() {
                 },
                 {
                   Name: 'email' /* required */,
-                  Value: 'test@test.com'
+                  Value: username || 'test@test.com'
                 }
                 /* more items */
               ]
@@ -82,7 +88,7 @@ function testUser() {
 
                 let user = data.User;
                 let params = {
-                  Password: 'passwordpassword' /* required */,
+                  Password: password || 'passwordpassword' /* required */,
                   UserPoolId: userPoolId /* required */,
                   Username: user.Username /* required */,
                   Permanent: true
@@ -92,7 +98,7 @@ function testUser() {
                   // an error occurred
                   else {
                     console.log(data); // successful response
-                    console.log(`User: '${user.Username}'; Email: 'test@test.com'; Password: 'passwordpassword' successfully created.`); // successful response
+                    console.log(`User: '${user.Username}'; Email: '${username || 'test@test.com'}'; Password: '${password || 'passwordpassword'}' successfully created.`); // successful response
                   }
                 });
               }
