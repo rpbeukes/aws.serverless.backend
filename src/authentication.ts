f9b6eb70919c2003139fc331992caa38645c093e
Ruan Beukes <ruan.coder@gmail.com>
2019-11-28 11:56:43 +0800

    fine grained access - patch loan works.

--- src/authentication.ts
+++ src/authentication.ts
@@ -1,11 +1,54 @@
 import { APIGatewayEventRequestContext } from 'aws-lambda';
 
-const getClaim = ({ authorizer }: Pick<APIGatewayEventRequestContext, 'authorizer' >, claim: string): string => {
+const getClaim = ({ authorizer }: Pick<APIGatewayEventRequestContext, 'authorizer'>, claim: string): string => {
     if (authorizer && authorizer.claims) {
         return authorizer.claims[claim];
     }
-    
+
     throw Error('Authorizer failed to supply user claims in request context');
 };
 
 export const getUsername = (requestContext: APIGatewayEventRequestContext) => getClaim(requestContext, 'email');
+
+export const isAdminUser = (requestContext: APIGatewayEventRequestContext) => {
+    const claim = getClaim(requestContext, 'cognito:groups')
+    // example claim: 'anotherGroup, admin'
+    return claim ? claim.includes('admin') : false;
+};
+
+/*
+Samples of the authorizer data:
+
+User in admin group:
+{
+    "claims": {
+        "sub": "10b4786a-a220-4476-90ce-df95cb18fe74",
+        "aud": "5aa218vcdflmohh3qhrtr0mj78",
+        "cognito:groups": "anotherGroup,admin",
+        "event_id": "638c4d7b-ccb3-41d5-a444-8ee5aca0248a",
+        "token_use": "id",
+        "auth_time": "1574911451",
+        "iss": "https://cognito-idp.ap-southeast-2.amazonaws.com/ap-southeast-2_AAPZcQ593",
+        "cognito:username": "10b4786a-a220-4476-90ce-df95cb18fe74",
+        "exp": "Thu Nov 28 04:24:11 UTC 2019",
+        "iat": "Thu Nov 28 03:24:11 UTC 2019",
+        "email": "admin@test.com"
+    }
+}
+    
+User in no group (just a regular user):
+{
+   "claims": {
+      "sub": "a83ef395-5a48-4fd8-a708-ad302aeaf0f9",
+      "aud": "5aa218vcdflmohh3qhrtr0mj78",
+      "event_id": "de18fd06-4d8b-4c12-b39f-5e31e6675628",
+      "token_use": "id",
+      "auth_time": "1574911450",
+      "iss": "https://cognito-idp.ap-southeast-2.amazonaws.com/ap-southeast-2_AAPZcQ593",
+      "cognito:username": "a83ef395-5a48-4fd8-a708-ad302aeaf0f9",
+      "exp": "Thu Nov 28 04:24:10 UTC 2019",
+      "iat": "Thu Nov 28 03:24:10 UTC 2019",
+      "email": "test@test.com"
+   }
+}
+*/
--- src/functions/loan/patchLoanById.ts
+++ src/functions/loan/patchLoanById.ts
@@ -3,9 +3,11 @@ import 'source-map-support/register';
 import { createLambdaHandler } from '../../middleware/shared-middleware-pipeline';
 import { createTableNameFromPrefix } from '../../shared/dynamoHelpers';
 import * as HttpStatus from 'http-status-codes';
-import { InternalServerError, NotFound, BadRequest } from 'http-errors';
+import { InternalServerError, NotFound, BadRequest, Forbidden
+ } from 'http-errors';
 import { Loan, PatchLoanModel } from '../../dataModels';
 import { loadById, patchUpdate } from '../../services/database';
+import { isAdminUser, getUsername } from '../../authentication';
 
 const lambda: APIGatewayProxyHandler = async event => {
   let response;
@@ -20,8 +22,8 @@ const lambda: APIGatewayProxyHandler = async event => {
       );
     }
 
-    let table = createTableNameFromPrefix('Loan');
-    let loan = await loadById<Loan>(table, event.pathParameters.id);
+    const table = createTableNameFromPrefix('Loan');
+    const loan = await loadById<Loan>(table, event.pathParameters.id);
 
     if (!loan) {
       throw new NotFound();
@@ -31,6 +33,21 @@ const lambda: APIGatewayProxyHandler = async event => {
 
     if (!model.status) {
       throw new BadRequest('failed to patch record - request body requires {"status": "submitted"| "approved" | "collected" | "returned" | "cancelled" } property');
+    }
+
+    const isAdministratorUser = isAdminUser(event.requestContext);
+    console.log(`isAdmin: ${isAdministratorUser}`);
+    
+    const user = getUsername(event.requestContext);
+    
+    const canUpdate = isAdministratorUser || user === loan.user;
+    if (!canUpdate) {
+      throw new Forbidden(`You do not have permission to modify this loan`);
+    }
+
+    const canUpdateWithCorrectStatus = isAdministratorUser || (model.status === 'submitted' || model.status === 'collected' || model.status === 'cancelled')
+    if (!canUpdateWithCorrectStatus) {
+      throw new Forbidden(`Invalid status: ${model.status}; Your privilege only allow loan status updates of: 'submitted', 'collected', 'cancelled'`);
     }
 
     let patchedLoan = await patchUpdate<Loan>(table, event.pathParameters.id, model);
