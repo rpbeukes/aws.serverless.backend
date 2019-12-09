27b22fa4ef6e5ff2bf2ef250c9ab7ff31789b82c
Ruan Beukes <ruan.coder@gmail.com>
2019-12-09 11:32:03 +0800

    add `Put` and `Update`permissions to ItemTable so that the lambda can add the records when reading it from the queue.

--- src/functions/sqs/importItems.ts
+++ src/functions/sqs/importItems.ts
@@ -1,10 +1,16 @@
 import { SQSHandler } from "aws-lambda";
+import { createTableNameFromPrefix } from "../../shared/dynamoHelpers";
+import { save } from "../../services/database";
+import { Item } from "../../dataModels";
 
 export const sqsHandler: SQSHandler = async (event) => {
-    console.log('sqsHandler invoked!');
     console.log(JSON.stringify(event, null, 2));
 
     for (const queueItem of event.Records) {
         console.log(queueItem.body);
+        const item = JSON.parse(queueItem.body) as Item;
+        let table = createTableNameFromPrefix('Item');
+        await save<Item>(table, item);
+        console.info(`Success - Saved record to Items table id: ${item.id}`);
     }
 };
